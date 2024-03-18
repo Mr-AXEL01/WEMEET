cb0a22452e9517fb1cd96d32babf06aa03562dec
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-18 23:26:08 +0000

    set up the migration for the groups

--- database/migrations/2024_03_18_224451_create_groups_table.php
+++ database/migrations/2024_03_18_224451_create_groups_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('cover_path')->nullable();
+            $table->string('thumbnail_path')->nullable();
+            $table->boolean('auto_approval')->default(true);
+            $table->text('about')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamp('deleted_at')->nullable();
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
             $table->timestamps();
         });
     }
