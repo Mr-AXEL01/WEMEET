63fcc08cea94fe72a4a047b7e3c529b30c332842
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-18 23:59:35 +0000

    set the migration for groups users

--- database/migrations/2024_03_18_224626_create_group_users_table.php
+++ database/migrations/2024_03_18_224626_create_group_users_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('group_users', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('status'); //approved, pending
+            $table->string('role'); //admin,user
+            $table->string('token')->nullable();
+            $table->timestamp('token_expire_date')->nullable();
+            $table->timestamp('token_used')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('group_id')->constrained('groups')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('created_by')->constrained('users');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
