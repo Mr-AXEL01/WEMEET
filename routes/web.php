a3d0483abaeae5724cc7ef9401b9c1a20328270c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-20 22:00:14 +0100

    create route for login and register to load the vue page and to get information to backend

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class,'index'])->name('home');
+
+//------------------AUTHENTIFICATION------------------
+Route::get('register', [RegisterController::class, 'create'])->name('register');
+Route::post('register', [RegisterController::class, 'store']);
+Route::get("/login", [LoginController::class, 'create'])->name("login");
+Route::post("/login", [LoginController::class, 'store']);
+
+
+
+Route::group(["middleware" => "auth"], function () {
+    Route::get('/', [HomeController::class,'index'])->name('home');
+    Route::get('/u/{username}', [HomeController::class,'index'])->name('profile');
+});
+
+
+
+
+
+
