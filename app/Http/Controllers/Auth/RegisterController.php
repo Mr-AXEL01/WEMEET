102d6cb71624f6bada4c98738f98f62e3d9621d4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-20 21:51:03 +0100

    create method to store new users and register them

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class RegisterController extends Controller
 {
     public function create() {
         return Inertia::render('Auth/Register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:users',
+            'password' => ['required', 'confirmed', min(8)],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->login($user);
+
+        $request->session()->regenerate()
+
+        return redirect()->route('home');
     }
 }
