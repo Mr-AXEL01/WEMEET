ac1aeceff9e9e913736e4e0d1fbc842cb0ddc5f0
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-21 12:28:58 +0100

    change the rules for the password verification and add he username to create a new user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,7 +10,8 @@ use Inertia\Inertia;
 
 class RegisterController extends Controller
 {
-    public function create() {
+    public function create()
+    {
         return Inertia::render('Auth/Register');
     }
 
@@ -19,12 +20,15 @@ class RegisterController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:users',
-            'password' => ['required', 'confirmed', min(8)],
+            'password' => 'required|confirmed|min:6',
         ]);
+
+        $username = $this->generateUsername($request->name);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'username' => $username,
             'password' => Hash::make($request->password),
         ]);
 
@@ -33,5 +37,18 @@ class RegisterController extends Controller
         $request->session()->regenerate();
 
         return redirect()->route('home');
+    }
+
+    private function generateUsername($name)
+    {
+        $cleanedName = strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $name));
+        $truncatedName = substr($cleanedName, 0, 10);
+        $username = $truncatedName;
+        $suffix = 1;
+        while (User::where('username', $username)->exists()) {
+            $username = $cleanedName . $suffix++;
+        }
+
+        return $username;
     }
 }
