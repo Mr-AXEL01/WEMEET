9c48e39a887d79b0d01f1757e9871e0a6b3009be
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-21 14:07:32 +0100

    create and index method to render the user to the view profile

---
+++ resources/js/Pages/Profile/view.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div>
+        <pre>
+            {{user}}
+        </pre>
+    </div>
+
+</template>
+
+<script setup>
+
+
+import {usePage} from "@inertiajs/vue3";
+
+const user = usePage().props.auth.user;
+
+</script>
