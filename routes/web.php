80d9c6c709eba144de42c7cd51102c8c5caffca4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-21 12:34:42 +0100

    set route for logout

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,12 @@ Route::get('register', [RegisterController::class, 'create'])->name('register');
 Route::post('register', [RegisterController::class, 'store']);
 Route::get("/login", [LoginController::class, 'create'])->name("login");
 Route::post("/login", [LoginController::class, 'store']);
-
+Route::post('/logout',[LoginController::class, 'destroy'])->name("logout");
 
 
 Route::group(["middleware" => "auth"], function () {
-    Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/', [HomeController::class,'index'])->name('home');
+
     Route::get('/u/{username}', [HomeController::class,'index'])->name('profile');
 });
 
