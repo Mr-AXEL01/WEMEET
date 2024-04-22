d50c4224c6943e4134e1109536de81ea4d329cf7
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-22 03:14:58 +0100

    set function to handle the recieze when transferring from mobilw to laptop

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,16 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import '../css/app.css';
 
+// ----------------start_of_Handling_Resize-------------
+function handleResize() {
+    if (window.innerWidth > 768){
+        window.scrollTo(0, 0);
+    }
+}
+window.addEventListener('resize', handleResize);
+
+// ----------------end_of_Handling_Resize-------------
+
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
