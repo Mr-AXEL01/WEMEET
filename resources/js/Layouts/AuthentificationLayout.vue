71d38e13023198a8c6258a2f8f73855403428eaa
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-22 09:19:04 +0100

    move the nav into the layout

--- resources/js/Layouts/AuthentificationLayout.vue
+++ resources/js/Layouts/AuthentificationLayout.vue
@@ -4,7 +4,36 @@
             <Link href="/">
                 <h1 class="font-bold text-2xl">WEMEET</h1>
             </Link>
-            <Nav />
+            <nav class="flex">
+                <ul v-if="!authUser" class="flex space-x-6">
+                    <li class="flex items-center space-x-1">
+                        <UserPlusIcon class="w-5 h-5"/>
+                        <Link href="/register" class="text-black hover:underline">
+                            Register
+                        </Link>
+                    </li>
+                    <li class="flex items-center space-x-1">
+                        <ArrowRightEndOnRectangleIcon class="w-5 h-5"/>
+                        <Link href="/login" class="text-black hover:underline">
+                            Login
+                        </Link>
+                    </li>
+                </ul>
+                <ul v-else class="flex space-x-6">
+                    <li class="flex items-center space-x-1">
+                        <UserCircleIcon class="w-5 h-5"/>
+                        <Link :href="'/u/' + authUser.username" class="text-black hover:underline">
+                            {{authUser.username}}
+                        </Link>
+                    </li>
+                    <li class="flex items-center space-x-1">
+                        <ArrowRightEndOnRectangleIcon class="w-5 h-5 rotate-180"/>
+                        <Link method="post" href="/logout" as="button" class="text-black hover:underline">
+                            Logout
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     </section>
 
@@ -16,9 +45,14 @@
 </template>
 
 <script setup>
-import Nav from '../Components/Nav.vue';
-import {Link} from "@inertiajs/vue3";
+import {Link, usePage} from "@inertiajs/vue3";
+import {UserPlusIcon , UserCircleIcon , ArrowRightEndOnRectangleIcon} from "@heroicons/vue/24/solid";
 
+const authUser = usePage().props.auth.user;
 
-
+defineProps({
+    user: {
+        type: Object
+    }
+})
 </script>
