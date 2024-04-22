bb1aab5ef80eeba94dd08aa29b05eeb0284d904c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-22 02:02:28 +0100

    create components for the button of the profile view list

---
+++ resources/js/Pages/Profile/Partials/TabItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <button
+        :class="['px-3 py-2.5 outline-none focus:outline-none text-sm',selected
+            ? 'bg-white text-blue-500 border-b-2 border-blue-500'
+            : 'text-gray-700',
+        ]"
+    >
+        {{ text }}
+    </button>
+</template>
+
+
+<script setup>
+defineProps({
+    selected: Boolean,
+    text: String,
+})
+</script>
