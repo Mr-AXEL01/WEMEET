eb59f58656285046fb4b987fc94b73243124d2e1
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-24 14:11:41 +0100

    style the main button

--- resources/js/Components/AlphaButton.vue
+++ resources/js/Components/AlphaButton.vue
@@ -7,7 +7,7 @@
     </Link>
     <button
         v-else
-        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-whit uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ease-in-out duration-150"
+        class="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ease-in-out duration-150"
     >
         <slot/>
     </button>
