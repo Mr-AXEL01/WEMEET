b744d166db18d5ba8d26b5d3ee20a0241c4d0aa6
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-24 11:54:32 +0100

    add the update method to handle the update ofthe informations of the user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProfileUpdateInformationRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class ProfileController extends Controller
@@ -13,6 +15,7 @@ class ProfileController extends Controller
     {
         return Inertia::render('Profile/view', [
             'status' => session('status'),
+            '$success' => session('$success'),
             'user' => new UserResource($user)
         ]);
     }
@@ -31,13 +34,36 @@ class ProfileController extends Controller
         /**  @var \Illuminate\Http\UploadedFile $cover */
 
         $cover = $data['cover'] ?? null;
-
+        $success = '';
         if($cover) {
-            $path = $cover->store('covers/'.$user->id, 'public');
+            if ($user->cover_path) {
+                Storage::disk('public')->delete($user->cover_path);
+            }
+            $path = $cover->store('user-'.$user->id, 'public');
             $user->update(['cover_path' => $path]);
+            $success = 'Your cover was been updated';
         }
-        session('success','Cover image has been updated successfully');
+        if($avatar) {
+            if ($user->avatar_path) {
+                Storage::disk('public')->delete($user->avatar_path);
+            }
+            $path = $avatar->store('user-'.$user->id, 'public');
+            $user->update(['avatar_path' => $path]);
+            $success = 'Your avatar was been updated';
+        }
 
-        return back()->with('status', 'cover-update');
+        return back()->with('success', $success);
+    }
+
+    /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateInformationRequest $request)
+    {
+        $request->user()->fill($request->validated());
+
+        $request->user()->save();
+
+        return to_route('profile', $request->user())->with('success', 'Your profile details were updated.');
     }
 }
