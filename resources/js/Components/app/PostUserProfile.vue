738abd882cb2a9d90425b76ad9c9e2bfb1c06672
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-25 22:55:09 +0100

    set the avatar profile and name and time of post in components to we can use in defferent place

---
+++ resources/js/Components/app/PostUserProfile.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+defineProps({
+    post: {
+        type: Object
+    },
+    showTime: {
+        type: Boolean,
+        default: true,
+    }
+})
+
+</script>
+
+<template>
+    <div class="flex items-center gap-2">
+        <a href="javascript:void(0)">
+            <img :src="post.user.avatar_url || '/img/default_avatar.jpg'"
+                 class="w-[40px] h-[40px] object-cover rounded-full border border-2 transition-all
+                     hover:border-blue-500"
+            />
+        </a>
+        <div>
+            <h4 class="font-bold">
+                <a href="javascript:void(0)" class="hover:underline">
+                    {{ post.user.name }}
+                </a>
+                <template v-if="post.group">
+                    >
+                    <a href="javascript:void(0)" class="hover:underline">
+                        {{ post.group.name }}
+                    </a>
+                </template>
+            </h4>
+            <small v-if="showTime" class="text-gray-400">
+                {{ post.created_at }}
+            </small>
+        </div>
+    </div>
+</template>
