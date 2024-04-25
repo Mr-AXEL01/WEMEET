04a748bcd05e3e66bdd93fa0b52d8a43632857e6
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-25 12:27:34 +0100

    set the fillable for the post and set relation with the user , group , attchments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -15,4 +17,19 @@ class Post extends Model
         'user_id',
         'body',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function attachments(): HasMany
+    {
+        return $this->hasmany(PostAttachment::class);
+    }
 }
