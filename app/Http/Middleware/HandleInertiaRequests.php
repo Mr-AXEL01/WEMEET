fde4424531de3ef071bbcddaef284de77f864f30
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-26 20:42:03 +0100

    set a statement to check if there is user in rquest so it will have the id from the resourceUrl otherwise it will take null in id

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,7 +40,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => new UserResource($request->user()),
+                'user' => $request->user() ? new UserResource($request->user()) : null,
             ],
             'ziggy' => fn () => [
                 ...(new Ziggy)->toArray(),
