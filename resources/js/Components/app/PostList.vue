2a00f75d8ed253b91e32e456d34894d8a322dcce
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-26 13:36:25 +0100

    set function to open the edit modal and pass the post object and set the show edit modal to true

--- resources/js/Components/app/PostList.vue
+++ resources/js/Components/app/PostList.vue
@@ -1,17 +1,29 @@
 <script setup>
 
 import PostItem from "./PostItem.vue";
+import PostModel from "./PostModel.vue";
+import {ref} from "vue";
 
 defineProps({
     posts: Array,
 });
 
+const showEditModal = ref(false)
+const editPost =ref({})
+
+function openEditModal(post){
+    editPost.value = post;
+    showEditModal.value = true;
+}
+
 </script>
 
 <template>
     <div class="overflow-auto lg:h-[77vh]">
-        <PostItem v-for="post of posts" :key="post.id" :post="post" />
+        <PostItem v-for="post of posts" :key="post.id" :post="post" @editClick="openEditModal" />
+        <PostModel :post="editPost" v-model="showEditModal" />
     </div>
+
 </template>
 
 <style scoped>
