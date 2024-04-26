bfda348d764ad74e6c2fddafc65ac40c13393e99
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-26 14:12:58 +0100

    set the deletePost function that show a confirmation message and route to post.delete to post and set the preserveScroll to stay in the same part and go back to the top of the page

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -3,6 +3,7 @@ import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/vue'
 import {PencilIcon, TrashIcon , EllipsisVerticalIcon} from '@heroicons/vue/20/solid'
 import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/vue";
 import PostUserProfile from "./PostUserProfile.vue";
+import { router } from "@inertiajs/vue3";
 
 
 const props = defineProps({
@@ -18,6 +19,14 @@ function isImage(attachment) {
 
 function openEditModal() {
     emit('editClick', props.post)
+}
+
+function deletePost() {
+    if(window.confirm('Are you sure you wanna delete this post?')) {
+        router.delete(route('post.destroy', props.post), {
+            preserveScroll: true
+        })
+    }
 }
 
 
@@ -68,6 +77,7 @@ function openEditModal() {
                             </MenuItem>
                             <MenuItem v-slot="{ active }">
                                 <button
+                                    @click="deletePost"
                                     :class="[
                   active ? 'bg-indigo-500 text-white' : 'text-gray-900',
                   'group flex w-full items-center rounded-md px-2 py-2 text-sm',
