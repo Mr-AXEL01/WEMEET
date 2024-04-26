c0855546b65454a4d22ecea36fb8fde0db4c512f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-26 12:21:06 +0100

    set the date of post to be on the last time that updated

--- resources/js/Components/app/PostUserProfile.vue
+++ resources/js/Components/app/PostUserProfile.vue
@@ -33,7 +33,7 @@ defineProps({
                 </template>
             </h4>
             <small v-if="showTime" class="text-gray-400">
-                {{ post.created_at }}
+                {{ post.updated_at }}
             </small>
         </div>
     </div>
